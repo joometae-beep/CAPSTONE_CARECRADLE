8dcf65d6f820d9826ed0c7bc32ea8f7454585606
joometae-beep <joome.tae@gmail.com>
2026-09-26 09:11:11 +0800

    Revert scheduler to daily 8AM production time

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:send-appointment-reminders')
-    ->everyMinute();
+->dailyAt('08:00');
